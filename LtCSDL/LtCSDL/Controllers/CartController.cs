260bc8271f04d05d08572925dd689b990717bc5d
Harry Vo BED <d.bed14@gptgdt.net>
2022-10-05 09:47:09 +0700

    release

---
+++ LtCSDL/LtCSDL/Controllers/CartController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using LtCSDL.Models;
+
+namespace LtCSDL.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        private DataClasses1DataContext dt = new DataClasses1DataContext();
+
+        public List<Cart> GetListCart()  //Lay DS gio hang
+        {
+            List<Cart> carts = Session["Cart"] as List<Cart>;
+            if (carts == null)
+            {
+                carts = new List<Cart>();
+                Session["Cart"] = carts;
+            }
+            return carts;
+        }
+
+        public ActionResult ListCarts()  //hien thi gio hang
+        {
+            List<Cart> carts = GetListCart();
+            if(carts.Count==0)  //gio hang chua co san pham
+            {
+                return RedirectToAction("ListProduct", "Product");
+            }
+            ViewBag.CountProduct = Count();
+            ViewBag.Total = Total();
+
+            return View(carts);
+        }
+
+        public ActionResult AddCart(int id)
+        {
+            List<Cart> carts = GetListCart(); //lay DSGH
+            Cart c = carts.Find(s => s.ProductID == id);
+            if(c == null)
+            {
+                c = new Cart(id); //tao SPGH moi
+                carts.Add(c);
+            }
+            else
+            {
+                c.Quantity++;
+            }
+            return RedirectToAction("ListCarts");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            List<Cart> carts = GetListCart();
+            Cart c = carts.Find( s => s.ProductID == id);
+
+            if(c!=null)
+            {
+                carts.RemoveAll(s => s.ProductID == id);
+                return RedirectToAction("ListProduct");
+            }
+            if (carts.Count == 0)
+            {
+                return RedirectToAction("ListProduct", "Product");
+            }
+            return RedirectToAction("ListCarts");
+        }
+
+        private int Count()
+        {
+            int n = 0;
+            List<Cart> carts = Session["Cart"] as List<Cart>;
+            if(carts != null)
+            {
+                n = carts.Sum(s => s.Quantity);
+            }
+            return n;
+        }
+
+        private decimal? Total()
+        {
+            decimal? total = 0;
+            List<Cart> carts = Session["Cart"] as List<Cart>;
+            if (carts != null)
+            {
+                total = carts.Sum(s => s.Total);
+
+            }
+            return total;
+        }
+
+        public ActionResult OrderProduct (FormCollection fCollection)
+        {
+            using (TransactionScope tranScope = new TransactionScope())
+            {
+                try
+                {
+                    Order order = new Order();
+                    List<Cart> carts = GetListCart();  //lay gio hang
+                    order.OrderDate = DateTime.Now;
+                    dt.Orders.InsertOnSubmit(order);
+                    dt.SubmitChanges();
+                    //order = dt.Orders.OrderByDescending(s => s.OrderID).Take(1).SingleOrDefault();
+                    foreach (var item in carts)
+                    {
+                        Order_Detail d = new Models.Order_Detail();
+                        d.OrderID = order.OrderID;
+                        d.ProductID = item.ProductID;
+                        d.Quantity = short.Parse(item.Quantity.ToString());
+                        d.UnitPrice = (decimal)item.UnitPrice;
+                        d.Discount = 0;
+
+                        dt.Order_Details.InsertOnSubmit(d);
+                    }
+                    dt.SubmitChanges();
+                    tranScope.Complete();
+                    Session["Cart"] = null;
+                }
+                catch (Exception)
+                {
+                    tranScope.Dispose();
+                    return RedirectToAction("ListCarts");
+                }
+            }
+            return RedirectToAction("OrderDetailList", "Cart");
+        }
+
+        public ActionResult OrderDetailList()
+        {
+            var p = dt.Order_Details.OrderByDescending(s => s.OrderID).Select(s => s).ToList();
+            return View(p);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LtCSDL/LtCSDL/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LtCSDL.Models;
+
+namespace LtCSDL.Controllers
+{
+    public class ProductController : Controller
+    {
+        DataClasses1DataContext da = new DataClasses1DataContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListProduct()
+        {
+            var ds = da.Products.Select(s => s).ToList();
+            return View(ds);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, Product product)
+        {
+            da.Products.InsertOnSubmit(product);
+            da.SubmitChanges();
+            return RedirectToAction("ListProducts");
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var p = da.Products.Where(s => s.ProductID == id).FirstOrDefault();
+            //var p = da.Products.FirstOrDefault();
+            return View(p);
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            var sp = da.Products.First(m => m.ProductID == id);
+            return View(sp);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            da.Products.DeleteOnSubmit(da.Products.First(s => s.ProductID == id));
+            da.SubmitChanges();
+            return RedirectToAction("ListProduct");
+        }
+        public ActionResult Edit(int id)
+        {
+            var p = da.Products.Where(s => s.ProductID == id).FirstOrDefault();
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FormCollection collection, int id)
+        {
+            var sp = da.Products.First(m => m.ProductID == id);
+            sp.ProductName = collection["ProductName"];
+            sp.UnitPrice = decimal.Parse(collection["UnitPrice"]);
+            sp.SupplierID = int.Parse(collection["SupplierID"]);
+            sp.UnitsInStock = short.Parse(collection["UnitsInStock"]);
+
+            UpdateModel(sp);
+            da.SubmitChanges();
+            return RedirectToAction("ListProduct");
+        }
+    }
+}
